8e01be42a2b4449387b755c0b5178af8528b82d2
ILYA <tuarone@gmail.com>
2023-07-30 22:03:08 +0200

    Задание: запуск автотестов для разных языков интерфейса

--- test_items.py
+++ test_items.py
@@ -9,3 +9,4 @@ def test_find_card_button(browser):
     time.sleep(30)
     add_to_basket_button = browser.find_elements(By.CSS_SELECTOR, ".btn-add-to-basket")
     assert len(add_to_basket_button) > 0, "Кнопка 'Добавить в корзину' не найдена на странице"
+
