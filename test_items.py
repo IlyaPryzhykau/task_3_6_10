e20f7967826ee351c9ac9ae95a37ddb7e5c1590b
ILYA <tuarone@gmail.com>
2023-07-30 21:57:40 +0200

    Задание: запуск автотестов для разных языков интерфейса

--- test_items.py
+++ test_items.py
@@ -4,7 +4,7 @@ import time
 link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
 
 
-def test_items(browser):
+def test_find_card_button(browser):
     browser.get(link)
     time.sleep(30)
     add_to_basket_button = browser.find_elements(By.CSS_SELECTOR, ".btn-add-to-basket")
