c6b9e464fef8e2923c15781688bf2d2a8b78c92e
ILYA <tuarone@gmail.com>
2023-07-30 21:37:00 +0200

    Задание: запуск автотестов для разных языков интерфейса

---
+++ test_items.py
@@ -0,0 +1,11 @@
+from selenium.webdriver.common.by import By
+import time
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+
+def test_items(browser):
+    browser.get(link)
+    time.sleep(30)
+    add_to_basket_button = browser.find_elements(By.CSS_SELECTOR, ".btn-add-to-basket")
+    assert len(add_to_basket_button) > 0, "Кнопка 'Добавить в корзину' не найдена на странице"
